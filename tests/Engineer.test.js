22902ba29d7b36443b5e8088f5b6d3c10de92e30
Alexander Barlow <alex.barlow@rocketmail.com>
2022-12-07 18:33:20 -0500

    created test suite for engineer and passed all tests

---
+++ tests/Engineer.test.js
@@ -0,0 +1,39 @@
+
+const Engineer = require('../lib/Engineer');
+
+describe('Engineer', () => {
+    describe('Initialization', () => {
+        it('should return an object with name, id, email and github', () => {
+            const engineer = new Engineer('Alex', 5, 'alex.barlow@rocketmail.com', 'AlexanderBarlow');
+
+        expect(engineer.name).toEqual('Alex');
+        expect(engineer.id).toEqual(5);
+        expect(engineer.email).toEqual('alex.barlow@rocketmail.com');
+        expect(engineer.github).toEqual('AlexanderBarlow');
+        });
+
+        it('should return an engineer.name', () => {
+            const engineer = new Engineer('Alex', 5, 'alex.barlow@rocketmail.com', 'AlexanderBarlow');
+
+        expect(engineer.name).toEqual('Alex');
+        });
+
+        it('should return engineer.id', () => {
+            const engineer = new Engineer('Alex', 5, 'alex.barlow@rocketmail.com', 'AlexanderBarlow');
+
+        expect(engineer.id).toEqual(5);
+        });
+
+        it('should return engineer.email', () => {
+            const engineer = new Engineer('Alex', 5, 'alex.barlow@rocketmail.com', 'AlexanderBarlow');
+
+        expect(engineer.email).toEqual('alex.barlow@rocketmail.com')
+        });
+
+        it('should return engineer.github', () => {
+            const engineer = new Engineer('Alex', 5, 'alex.barlow@rocketmail.com', 'AlexanderBarlow');
+
+        expect(engineer.github).toEqual('AlexanderBarlow');
+        });
+    })
+});
